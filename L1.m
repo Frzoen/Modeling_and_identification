75fceddf3a58b267dde311e2b6a21fb19bd9be4d
Frozen <mrozix11@gmail.com>
2018-02-27 16:24:32 +0100

    Second laboratory

---
+++ L1.m
@@ -0,0 +1,25 @@
+close all;
+clear all;
+seed = 0.85;
+random_number_sequence_x = [];
+random_number_sequence_y = [];
+random_number_x = seed;
+random_number_y = 1-seed;
+random_number_sequence_x = [random_number_sequence_x; random_number_x-0.5];
+random_number_sequence_y = [random_number_sequence_y; random_number_y-0.5];
+count = 0;
+
+for i = 1:1:1000
+    random_number_x = sawtooth(100*random_number_x)/2 + 0.5;
+    random_number_sequence_x = [random_number_sequence_x; random_number_x-0.5];
+    random_number_y = sawtooth(100*random_number_x)/2 + 0.5;
+    random_number_sequence_y = [random_number_sequence_y; random_number_y-0.5];
+    if(sqrt(random_number_x^2+random_number_y^2) <= 0.5)
+        count = count + 1;
+    end
+end
+
+plot(random_number_sequence_x, random_number_sequence_y,'*')
+viscircles([0,0],1/2);
+ans = count/1000;
+ans
