44115dc4138a793b075831dd2a6b12cd7ce6bbd3
Frozen <mrozix11@gmail.com>
2018-04-19 14:41:41 +0200

    Sawtooth and sine generating functions with comments

--- L1.m
+++ L1.m
@@ -1,24 +1,32 @@
 %sin() and saw() random number generators
 
-close all;
+close all; % cleaning
 clear all;
 
-number_of_samples = 5000;
+number_of_samples = 5000; % fixed numer of samples
 
-seed = 0.712;
-seed2 = 0.813;
-random_number_sequence_x = zeros(1,number_of_samples);
+seed = 0.712;  % initial seed for random number generator
+seed2 = 0.813; % initial seed for random number generator
+
+% initialize vectors for random numbers
+random_number_sequence_x = zeros(1,number_of_samples); 
 random_number_sequence_y = zeros(1,number_of_samples);
+
+% chose which sequence is geting which seed
 random_number_x = seed;
 random_number_y = seed2;
+
+% set first value in sequence
 random_number_sequence_x(1) = random_number_x;
 random_number_sequence_y(1) = random_number_y;
 
+% counter for montecarlo methode
 count = 0;
 
+% generate linear space of ponints for plot
 x = linspace(-1,1,number_of_samples);
-data = sawtooth(x*15);
 
+% random number generating loop
 for i = 2:1:number_of_samples
     random_number_x = sawtooth(15*random_number_x*pi)/2+0.5;
     random_number_sequence_x(i) = random_number_x;
@@ -28,9 +36,14 @@ for i = 2:1:number_of_samples
         count = count + 1;
     end
 end
+% generate figure for plots
 figure(1);
+% choose plot in figure
 subplot(2,1,1);
+
 plot(random_number_sequence_y, random_number_sequence_x,'*')
+
+% generating legend for plot
 title('Sawtooth function','fontsize',14);
 hold on;
 viscircles([0.5,0.5],0.5);
@@ -79,7 +92,6 @@ end
 plot(random_number_sequence_y, random_number_sequence_x,'*')
 title('Sin function','fontsize',14);
 viscircles([0,0],1);
-ans = count/number_of_samples;
-ans
+count/number_of_samples
 
 %plot(x,data)
